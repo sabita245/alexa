0fcb53d5d9727337806ace4f26d792a92dc8d0c9
sabita <sabita.rani1@gmail.com>
2021-06-24 06:25:35 +0530

    added one print statement

--- helo.java
+++ helo.java
@@ -1,6 +1,7 @@
 // This will print hello on command line
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("Hello, git");	
     }
 }
